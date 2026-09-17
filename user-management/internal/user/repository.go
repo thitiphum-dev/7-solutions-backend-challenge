5055a61cce06c149ccba15f37aeda7857b112700
stacktemple <stacktemple@gmail.com>
2026-09-17 18:27:58 +0700

    feat(user-management): add user domain and repository port

---
+++ user-management/internal/user/repository.go
@@ -0,0 +1,16 @@
+package user
+
+import "context"
+
+type Repository interface {
+	Create(ctx context.Context, user *User) error
+
+	FindByEmail(ctx context.Context, email string) (*User, error)
+	FindByID(ctx context.Context, id string) (*User, error)
+	List(ctx context.Context) ([]*User, error)
+
+	Update(ctx context.Context, user *User) error
+	Delete(ctx context.Context, id string) error
+
+	Count(ctx context.Context) (int64, error)
+}
